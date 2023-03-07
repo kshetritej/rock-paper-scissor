0a41945cda053a3ed0dd5062af6ba60df01c9147
kshetritej <furiousdtej@gmail.com>
2023-03-07 21:24:04 +0545

    write logic for declaring winner and loser of single round

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 function getcomputerChoice(){
     const computerSelection = (Math.floor(Math.random() * 3))
     switch(computerSelection){
@@ -9,3 +12,49 @@ function getcomputerChoice(){
             return 'scissor'
     }
 }
+
+
+function playRound(computerSelection, playerSelection){
+
+    if(computerSelection == 'rock' && playerSelection == 'paper' ||
+    computerSelection == 'paper' && playerSelection == 'scissor' ||
+    computerSelection == 'scissor' && playerSelection == 'rock')
+    {
+        playerScore++;
+        return `You won, ${playerSelection} beats ${computerSelection} \nYou: ${playerScore}, Computer: ${computerScore}`;
+    }
+
+    else if(computerSelection == 'rock' && playerSelection == 'scissor'||
+    computerSelection == 'paper' && playerSelection == 'rock' ||
+    computerSelection == 'scissor' && playerSelection == 'paper')
+    {   
+        computerScore++;
+        return `Computer won, ${computerSelection} beats ${playerSelection} \nYou: ${playerScore}, Computer: ${computerScore}`;
+    }
+    else if(computerSelection === playerSelection){
+        return `It's a draw. \nYou: ${playerScore}, Computer: ${computerScore}`
+    }
+    else{
+        return `Input ERR`;
+    }
+
+}
+
+
+function game(){
+    for(let i = 1; i<=5; i++){
+        console.log(playRound(getcomputerChoice(),'paper'));
+    }
+    if(computerScore > playerScore){
+            console.log('Computer won this round.');
+    }
+    else if(playerScore > computerScore){
+        console.log('Congratulations! you won this round.');
+    }
+    else{
+        console.log("Uff! what a tough game.");
+    }
+}
+
+
+game();
